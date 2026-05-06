e7ef40b7b9e285ef11111239e606fb957bb79fb2
DavidRobertJohns <david@davidmakes.com>
2026-05-06 19:52:03 +1000

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'system-ui, sans-serif', padding: 32 }}>
+      <h1>Day&apos;s Test API</h1>
+      <p>Try <a href="/api/daily-test?lang=en">/api/daily-test?lang=en</a></p>
+    </main>
+  );
+}
